8f8de4832079cb5ad214a778de0149ffc961fae8
Josh Grant <josh@synergitech.co.uk>
2022-10-21 14:21:50 +0100

    add default retry middleware

--- src/HttpClientFactory.php
+++ src/HttpClientFactory.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace Postal;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 
 class HttpClientFactory
 {
     public static function create(string $host, string $apiKey, callable $handler = null): Client
     {
+        if ($handler === null) {
+            $handler = HandlerStack::create();
+            $handler->push(RetryMiddlewareFactory::build());
+        }
+
         return new Client([
             'base_uri' => "{$host}/api/v1/",
             'headers' => [
